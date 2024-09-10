c76e68b2639fb822da78b27d022288776194f3b5
Diana Butiakova <butyakova01@mail.ru>
2024-09-11 01:47:45 +0600

    init: configure webpack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -9,3 +10,4 @@ function App() {
 }
 
 export default App;
+
---
+++ frontend/webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.tsx'),
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: process.env.NODE_ENV || 'development',
+  devtool: process.env.NODE_ENV ? false : 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|tsx)?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/env",
+              "@babel/react",
+              "@babel/typescript"
+            ]
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|txt|csv|mmdb|jpg|png|svg|ico)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+    ],
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, "public", "index.html"),
+      }),
+  ],
+  resolve: {
+    extensions: [".js", ".tsx"],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
