3c2f3325e14184706836db012720e5721e04ec3c
Diana Butiakova <butyakova01@mail.ru>
2024-09-11 00:43:25 +0600

    init: create react project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">Личный кабинет продавца</header>
+    </div>
+  );
+}
+
+export default App;
