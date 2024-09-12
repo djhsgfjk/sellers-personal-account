8d32254ad59c9e7b948410c1db604e11c4586e25
Diana Butiakova <butyakova01@mail.ru>
2024-09-12 13:13:11 +0600

    feat: open modalWindow in orderCard

--- frontend/src/components/OrderCard.tsx
+++ frontend/src/components/OrderCard.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Order } from '../types';
+import ModalWindow from './ModalWindow';
 
 function OrderCard({ order } : { order: Order }) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
     // Номер заказа;
     // Дата создания заказа;
     // Статус (текстом);
@@ -24,8 +31,14 @@ function OrderCard({ order } : { order: Order }) {
                 {order.finishedAt ? <div><b>Дата завершения:</b> {new Date(Date.parse(order.finishedAt)).toLocaleDateString()}</div>
                 : <div><b>Возможность завершения:</b> Да</div>}
             </Card.Text>
-            <Button variant="primary">Показать все товары ({order.items.length})</Button>
+            <Button variant="primary" onClick={handleShow}>Показать все товары ({order.items.length})</Button>
           </Card.Body>
+          <ModalWindow
+            title={'Товары заказа №' + order.id}
+            showButtons={false}
+            handleClose={handleClose}
+            show={show}
+          ></ModalWindow>
         </Card>
       );
 }
