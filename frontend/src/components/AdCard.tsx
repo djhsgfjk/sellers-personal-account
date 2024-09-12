c9d55db5f50263946165ecbb85ea52dcf2c125cb
Diana Butiakova <butyakova01@mail.ru>
2024-09-12 12:38:48 +0600

    feat: update adCard design

--- frontend/src/components/AdCard.tsx
+++ frontend/src/components/AdCard.tsx
@@ -10,14 +10,14 @@ function AdCard({ ad } : { ad: Ad }) {
     // Количество лайков;
 
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card style={{ width: '14rem' }}>
           <Card.Img variant="top" src={ad.imageUrl} alt={ad.name} />
           <Card.Body>
             <Card.Title>{ad.name}</Card.Title>
+            <Card.Subtitle>{ad.price}₽</Card.Subtitle>
             <Card.Text>
-                <div><b>Стоимость:</b> <span>{ad.price}</span></div>
-                <div><b>Просмотры:</b> <span>{ad.views}</span></div>
-                <div><b>В избранном:</b> <span>{ad.likes}</span></div>
+                <div><i className="bi bi-eye-fill"></i> {ad.views}</div>
+                <div><i className="bi bi-heart-fill"></i> {ad.likes}</div>
             </Card.Text>
           </Card.Body>
         </Card>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './index.css';
 import App from './App';
 import Ads from './components/Ads';
